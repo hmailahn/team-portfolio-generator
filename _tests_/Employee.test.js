e11faedc9d697f29ec44d1cb2cd145e7bb971b5f
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-01-30 12:51:30 -0600

    added initial files, npm, inquirer, and jest. Ran first test in employee test and added constructor to employee.js

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,11 @@
+
+const checkIfEqual = require('../lib/Employee.js');
+
+test('creates employee object', () => {
+    const employee = new Employee('Dave', '1', 'dave@email.com');
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toBe('1');
+    expect(employee.email).toBe('dave@email.com');
+});
+
